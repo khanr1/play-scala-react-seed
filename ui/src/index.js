ddce2b24091aba64272f531359b478aa316bed8d
khanr1 <raphael.khan@gmail.com>
2021-11-22 10:51:00 +0200

    adding Sass loader

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HW from './assets/hw.png'
+import './styles/index.css';
+import './styles/index.scss'
+
 
 ReactDOM.render(
     <>
-    <h1>Hello World!</h1>
-    <p> if you see this it means that react is working</p>
+    <h1 class='cssTest'>Hello World!</h1>
+    <p class='scssTest'> if you see this it means that react is working</p>
     <img src={HW} alt='La maison jungle' />
 
     </>,
